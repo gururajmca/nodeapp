98dbdccf48e3f0f7fd54f3684011d1aca12e15c7
Gururaj Naik <gururaj@netskope.com>
2021-03-21 22:23:50 +0530

    Removed mysql connection

--- server.js
+++ server.js
@@ -1,25 +1,13 @@
 var express    = require("express");
-// var login = require('./routes/loginroutes');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
-// app.use(express.static(__dirname + 'promises')); //Serves resources from promises folder
 var router = express.Router();
 // test route
 router.get('/', function(req, res) {
     res.json({ message: 'welcome to our upload module apis' });
 });
-//route to handle user registration
-// router.post('/register',login.register);
-// router.post('/login',login.login)
-// router.get('/test',login.test)
-// app.use('/api', router);
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
