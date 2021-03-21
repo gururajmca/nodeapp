7a91df4b5cd02a8db83091d57a77219982d31ac9
Gururaj Naik <gururaj@netskope.com>
2021-03-21 16:09:19 +0530

    Added Data Fetch calls code

--- promises/fetchdata.js
+++ promises/fetchdata.js
@@ -1,5 +1,24 @@
-
-const fetch = require("node-fetch");
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json))
+const url = 'https://jsonplaceholder.typicode.com/users';
+let h = new Headers();
+h.append('Accept', 'application/json');
+const req = new Request(url, {
+  method: 'GET',
+  headers: h,
+  mode: 'cors'
+});
+fetch(req)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error('BAD Request headers');
+      }
+    })
+    .then(json => {
+        console.log('Users list ', json);
+        let appendText = document.getElementById('output');
+        appendText.textContent = JSON.stringify(json);
+    })
+    .catch((err) => {
+        console.log('Error:', err.message);
+    });
--- server.js
+++ server.js
@@ -4,11 +4,12 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+// app.use(express.static(__dirname + 'promises')); //Serves resources from promises folder
 var router = express.Router();
 // test route
 router.get('/', function(req, res) {
@@ -25,4 +26,16 @@ app.get('/', (req, res) => {
 app.get('/promise', (req, res) => {
   res.send('Hello promise!')
 })
+app.get('/home', (req, res) => {
+    res.sendFile('./templates/home.html', { root: __dirname });
+});
+app.get('/promises/promises.js', (req, res) => {
+    res.sendFile('./promises/promises.js', { root: __dirname });
+});
+app.get('/promises/fetchdata.js', (req, res) => {
+    res.sendFile('/promises/fetchdata.js', { root: __dirname });
+});
+app.get('/about', (req, res) => {
+    res.sendFile('./templates/about.html', { root: __dirname });
+});
 app.listen(3000, function() { console.log('Running @ 3000'); } );
